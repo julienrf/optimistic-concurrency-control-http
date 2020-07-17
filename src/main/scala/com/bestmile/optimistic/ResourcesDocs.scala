2d706b3d6895891902db985eba0cdfdde9f5a2a8
Julien Richard-Foy <julien@richard-foy.fr>
2020-07-17 13:07:48 +0200

    add explcit result type

--- src/main/scala/com/bestmile/optimistic/ResourcesDocs.scala
+++ src/main/scala/com/bestmile/optimistic/ResourcesDocs.scala
@@ -1,11 +1,11 @@
 package com.bestmile.optimistic
 
 import endpoints4s.openapi
-import endpoints4s.openapi.model.Info
+import endpoints4s.openapi.model.{Info, OpenApi}
 
 object ResourcesDocs extends ResourcesApi with openapi.Endpoints with openapi.JsonEntitiesFromSchemas {
 
-  val api = openApi(
+  val api: OpenApi = openApi(
     Info("Optimistic Concurrency Control", "1.0.0")
   )(read, update)
 
